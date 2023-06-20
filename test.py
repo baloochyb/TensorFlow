281bc0b6f38c93aec8d76c03b86f1967e4249def
Behnam Baloochy <baloochy@gmail.com>
2023-06-20 08:51:02 +0330

    test

---
+++ test.py
@@ -0,0 +1,46 @@
+"""Docstring"""
+import tensorflow as tf
+
+# print("TensorFlow version: {}".format(tf.__version__))
+# print("Eager execution is: {}".format(tf.executing_eagerly()))
+# print("Keras version: {}".format(tf.keras.__version__))
+
+# var = tf.Variable([3, 3])
+
+"""print(tf.config.list_physical_devices('GPU'))
+if tf.test.is_gpu_available():
+    print('Running on GPU')
+    print('GPU #0?')
+    print(var.device.endswith('GPU:0'))
+else:
+    print('Running on CPU')"""
+
+# t1 = tf.Variable(42)
+# t2 = tf.Variable([ [ [0., 1., 2.], [3., 4., 5.] ], [ [6., 7., 8.], [9., 10., 11.] ] ])
+# print(t1, t2)
+
+# f64 = tf.Variable(89, dtype=tf.float64)
+# print(f64)
+# f64.assign(98.)
+# print(f64)
+
+# m_o_l = tf.constant(42)
+# print(m_o_l)
+# print(m_o_l.numpy())
+# unit = tf.constant(1, dtype = tf.int64)
+# print(unit)
+
+t2 = tf.Variable([ [ [0., 1., 2.], [3., 4., 5.] ], [ [6., 7., 8.], [9., 10., 11.] ] ])
+# print(t2.shape)
+
+# r1 = tf.reshape(t2, [2, 6])
+# r2 = tf.reshape(t2, [1, 12])
+# print(r1)
+# print(r2)
+
+# print(tf.rank(t2))
+# t3 = t2[1, 0, 2] # slice 1, row 0, column 2
+# print(t3)
+
+# print(t2.numpy())
+print(t2[1, 0, 2].numpy())
